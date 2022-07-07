4a4b9bf65a8327794837096b0ab4ec4e6e3767ba
Thong Toryod <87599310+myrobotstemiot@users.noreply.github.com>
2022-07-07 14:05:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ module.exports = {
     name : 'MyESP324WD',
     platform : 'arduino-esp32',
     title : 'MyESP324WD',
-    description : 'MyESP324WDบอร์ดเพื่อการเรียนรู้ด้านหุ่นยนต์ เป็นสื่อกลางในการเรียนรู้การเขียนโปรแกรมควบคุมหุ่นยนต์พื้นฐานจนกระทั่งขั้นสูง บอร์ดนี้ออกแบบมาให้ขับเคลื่อน 4ล้อ.\n',
+    description : 'MyESP32-4WD บอร์ดเพื่อการเรียนรู้ด้านหุ่นยนต์ เป็นสื่อกลางในการเรียนรู้การเขียนโปรแกรมควบคุมหุ่นยนต์พื้นฐานจนกระทั่งขั้นสูง บอร์ดนี้ออกแบบมาให้ขับเคลื่อน 4ล้อ.\n',
     author : 'kruthong',
     website : 'http://www.myrobotstem.com/',
     email : 'kruthong@gmail.com',
-    git : 'https://github.com/myrobotstemiot/MyESP32v4WD/',
+    git : 'https://github.com/myrobotstemiot/MyESP324WD/',
     image : '/static/display.jpg',
     version : '0.2.0'
 };
